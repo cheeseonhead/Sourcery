e996b949c5d0bd40353a404fa7ec66163ac35372
Jeffrey Wu <jeffwu@apple.com>
2021-05-08 15:09:05 -0700

    Do not replace the return type name of initializers

--- SourceryRuntime/Sources/Composer.swift
+++ SourceryRuntime/Sources/Composer.swift
@@ -541,23 +541,6 @@ public enum Composer {
 
         if method.isInitializer || method.isFailableInitializer {
             method.returnType = definedInType
-            if let type = method.actualDefinedInTypeName {
-                if method.isFailableInitializer {
-                    method.returnTypeName = TypeName(
-                        name: type.name,
-                        isOptional: true,
-                        isImplicitlyUnwrappedOptional: false,
-                        tuple: type.tuple,
-                        array: type.array,
-                        dictionary: type.dictionary,
-                        closure: type.closure,
-                        generic: type.generic,
-                        isProtocolComposition: type.isProtocolComposition
-                    )
-                } else if method.isInitializer {
-                    method.returnTypeName = type
-                }
-            }
         } else {
             method.returnType = resolve(method.returnTypeName, type)
         }
